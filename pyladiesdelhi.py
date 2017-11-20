402b84a4e390327350d4fda99284a3b09bc18ea5
Shashank Kumar <abc@shanky.xyz>
2017-11-21 00:29:25 +0530

    Update pyladiesdelhi.py

--- pyladiesdelhi.py
+++ pyladiesdelhi.py
@@ -24,12 +24,30 @@ def invitelink(bot,update):
         sleep(0.2)
         bot.sendMessage(chat_id=update.message.chat_id, text='https://t.me/joinchat/C_ZAjhGaj0SshHl6woLvuA')
 
+def twitter(bot,update):
+        bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+        sleep(0.2)
+        bot.sendMessage(chat_id=update.message.chat_id, text='https://twitter.com/pyladiesDL')
+
+def github(bot,update):
+        bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+        sleep(0.2)
+        bot.sendMessage(chat_id=update.message.chat_id, text='https://github.com/PyLadiesDelhi')
+
+def email(bot,update):
+        bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+        sleep(0.2)
+        bot.sendMessage(chat_id=update.message.chat_id, text='delhi@pyladies.com')
+
 def help(bot, update):
-	bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-	sleep(0.2)
-	bot.sendMessage(chat_id=update.message.chat_id, text='''
+        bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+        sleep(0.2)
+        bot.sendMessage(chat_id=update.message.chat_id, text='''
 Use one of the following commands
 /invitelink - to get an invite link for PyLadies Delhi Telegram Group of Volunteers
+/twitter - to get the twitter profile link of PyLadies Delhi
+/github - to get github profile link of PyLadies Delhi
+/email - to get email address in order to contact PyLadies Delhi
 /help - to see recursion in action ;)
 
 To contribute to|modify this bot : https://github.com/realslimshanky/PyLadiesDelhiBot
@@ -37,6 +55,7 @@ To contribute to|modify this bot : https://github.com/realslimshanky/PyLadiesDel
 
 dispatcher.add_handler(CommandHandler('start', start, pass_args=True))
 dispatcher.add_handler(CommandHandler('invitelink', invitelink))
+dispatcher.add_handler(CommandHandler('twitter', twitter))
+dispatcher.add_handler(CommandHandler('github', github))
+dispatcher.add_handler(CommandHandler('email', email))
 dispatcher.add_handler(CommandHandler('help', help))
-
-updater.start_polling()
