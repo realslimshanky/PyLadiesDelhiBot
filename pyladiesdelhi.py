b534d9b19f908b30e6f0aa3f4d9fc87c3ebd33de
Shashank Kumar <abc@shanky.xyz>
2017-12-04 23:06:44 +0530

    Process and Token/Key management added

--- pyladiesdelhi.py
+++ pyladiesdelhi.py
@@ -1,13 +1,68 @@
-from telegram.ext import Updater,CommandHandler
+from telegram.ext import Updater, CommandHandler
 from telegram import ChatAction
 from datetime import datetime, timedelta
 from pytz import timezone
 from time import sleep
-import logging,requests,pytz,re,ast
+import logging
+import requests
+import pytz
+import re
+import ast
+import os
+import json
+import sys
+import signal
+import subprocess
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-updater=Updater(token='<Bot-Token>')
+"""
+---Process ID Management Starts---
+This part of the code helps out when you want to run your program in background using '&'. This will save the process id of the program going in background in a file named 'pid'. Now, when you run you program again, the last one will be terminated with the help of pid. If in case the no process exist with given process id, simply the `pid` file will be deleted and a new one with current pid will be created.
+"""
+currentPID = os.getpid()
+if 'pid' not in os.listdir():
+    with open('pid', mode='w') as f:
+        print(str(currentPID), file=f)
+else:
+    with open('pid', mode='r') as f:
+        try:
+            os.kill(int(f.read()), signal.SIGTERM)
+            print("Terminating previous instance of " +
+                  os.path.realpath(__file__))
+        except ProcessLookupError:
+            subprocess.run(['rm', 'pid'])
+    with open('pid', mode='w') as f:
+        print(str(currentPID), file=f)
+"""
+---Process ID Management Ends---
+"""
+
+"""
+---Token/Key Management Starts---
+This part will check for the config.txt file which holds the Telegram and Meetup Token/Key and will also give a user friendly message if they are invalid. New file is created if not present in the project directory.
+"""
+configError = "Please open config.txt file located in the project directory and relace the value '0' of Telegram-Bot-Token with the Token you recieved from botfather and similarly for Meetup-API-Key"
+if 'config.txt' not in os.listdir():
+    with open('config.txt', mode='w') as f:
+        json.dump({'Telegram-Bot-Token': 0, 'Meetup-API-Key': "None"}, f)
+        print(configError)
+        sys.exit(0)
+else:
+    with open('config.txt', mode='r') as f:
+        config = json.loads(f.read())
+        if config["Telegram-Bot-Token"] or config["Meetup-API-Key"]:
+            print("Token Present, continuing...")
+            TelegramBotToken = config["Telegram-Bot-Token"]
+            MeetupAPIKey = config["Meetup-API-Key"]
+        else:
+            print(configError)
+            sys.exit(0)
+"""
+---Token/Key Management Ends---
+"""
+
+updater=Updater(token=TelegramBotToken)
 dispatcher=updater.dispatcher
 
 print("I'm On..!!")
